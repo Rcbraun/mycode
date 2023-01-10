2e60df2572a368b7c87b3a93c198c25d1317db52
Rcbraun <rcbraun.2@gmail.com>
2023-01-10 17:46:39 +0000

    env reset2

--- challenges/iss_tracker.py
+++ challenges/iss_tracker.py
@@ -5,7 +5,7 @@
 import json
 import requests
 import datetime as dt
-
+import reverse_geocoder as rg
 
 def main():
 
@@ -20,14 +20,24 @@ def main():
     print (iss)
     epoch_time = iss['timestamp']
     date = dt.datetime.fromtimestamp(epoch_time)
-    longitude= iss ['iss_position'('longitude')]
-    latitude = iss ['latitude']
-
+    
     print('CURRENT LOCATION OF THE ISS')
     print(f'Timestamp: {date}')
-    print(f'Longitude: {longitude}')
-    print(f'Latitude: {latitude}')
+    print(f"Longitude: {iss.get('iss_position').get('longitude')}")
+    print(f"Latitude: {iss.get('iss_position').get('latitude')}")
 
+
+
+    lat= iss.get('iss_position').get('latitude')
+    lon= iss.get('iss_position').get('longitude')
+
+    # reverse_geocoder MUST be passed a tuple as the argument!
+    coords_tuple= (lat, lon)
+
+    result = rg.search(coords_tuple, verbose=False)
+                                 # verbose=False removes an annoying output that reads 
+                                 # "Loading formatted geocoded file..."
+    print(result[0].get('name') , result[0].get('cc'))
 
 
 main()
